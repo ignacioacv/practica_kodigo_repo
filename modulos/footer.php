75d07756f265cd122502fa73ad438186e77581c9
Ignacio Crotez <ignacio.cortez@kodigo.org>
2024-01-21 23:05:28 -0600

    Poryecto-kodigo-php

---
+++ modulos/footer.php
@@ -0,0 +1,14 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+    <div class="container">
+        <div class="copyright">
+            &copy; Copyright <strong><span>2023</span></strong>
+        </div>
+        <div class="credits">
+            Designed by <a href="#">Kenia Paiz/FSJ19</a>
+        </div>
+    </div>
+</footer><!-- End  Footer -->
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+        class="bi bi-arrow-up-short"></i></a>
